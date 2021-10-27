dea42b6491d62496c92629e05e4bd68a1b0e89be
samuelshan123 <samuelshan123@gmail.com>
2021-10-27 23:17:20 +0530

    validate

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPage implements OnInit {
 
-  constructor() { }
+
+  regForm: FormGroup;
+  submitted = false;
+
+  
+  
+
+  constructor(  
+    private router: Router,public formBuilder: FormBuilder,public toastCtrl:ToastController
+    ) {
+     }
 
   ngOnInit() {
+  
+    this.regForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(4)]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+     email: ['', [Validators.required, Validators.pattern('[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$')]],
+     phone: ['', [Validators.required, Validators.pattern('^[0-9]+$')]],
+     type: ['', [Validators.required]],
+    //  servicename:['',[Validators.required]],
+    //  orgname: ['', [Validators.required]],
+    //  pan:['',[Validators.required]]
+   })
   }
+ 
+  
+check(){
+  console.log('checked');
+  
+  this.regForm= this.formBuilder.group({
+    
+      servicename:['',[Validators.required]],
+      orgname: ['', [Validators.required]],
+      pan:['',[Validators.required]]
+  })
 
 }
+  get errorCtr() {  
+    return this.regForm.controls;
+    
+  }
+
+
+  async openToast() {  
+    const toast = await this.toastCtrl.create({  
+      message: 'Logging in Please wait.....',
+            duration: 2000  
+
+    });  
+    toast.present();  
+    toast.onDidDismiss().then((val) => {  
+      console.log('Toast Dismissed');  
+    });  
+  }  
+  async openSucessToast() {  
+    const toast = await this.toastCtrl.create({  
+      message: 'Registered SuccessFull...',
+            duration: 2000  
+
+    });  
+    toast.present();  
+    toast.onDidDismiss().then((val) => {  
+      console.log('Toast Dismissed');  
+    });  
+  }  
+  async openFailToast() {  
+    const toast = await this.toastCtrl.create({  
+      message: 'Register Failed!!!',
+            duration: 2000  
+
+    });  
+    toast.present();  
+    toast.onDidDismiss().then((val) => {  
+      console.log('Toast Dismissed');  
+    });  
+  }  
+
+  onSubmit() {
+    this.submitted = true;
+    if (!this.regForm.valid) {
+      console.log('All fields are required.')
+      return false;
+    } else {
+      this.openToast();
+      console.log(this.regForm.value)     
+    }
+  }
+  
+
+
+}
+
