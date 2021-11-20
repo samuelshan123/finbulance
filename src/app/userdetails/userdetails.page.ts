6238dadc0af391f6d46f84b0e8d91813b8cf22d4
samuelshan123 <samuelshan123@gmail.com>
2021-11-20 10:16:50 +0530

    user details page

--- src/app/userdetails/userdetails.page.ts
+++ src/app/userdetails/userdetails.page.ts
@@ -17,15 +17,10 @@ export class UserdetailsPage implements OnInit {
   // cab:any =JSON.parse(localStorage.getItem("cab"))
   data:any=this.api.sharedData;
 
-     Details:any[] =['Name : '+this.data.user_id.name,
-     'E-mail : '+this.data.user_id.email,
-     'Pick-up Place : '+this.data.pickup,
-     'Pick-up Date & time : '+this.data.pickup_date,
-     'No of Persons : '+this.data.person,
-     'Vehicle : '+this.data.vehicle,
-     'Destination : '+this.data.destination,
-    "Trip type : "+this.data.trip,
-  "Return Date : "+this.data.return_date]
+  service=localStorage.getItem("service");
+
+
+     Details:any[] 
     //  Fields:any[];
     //  Values:any;
 
@@ -36,23 +31,57 @@ export class UserdetailsPage implements OnInit {
 
     //  }
 
-    
-   
-
   constructor(private formBuilder: FormBuilder,private api:ApiService){
     // for(const key in this.User) {
     // console.log(`${key} : ${this.User[key]}`);
           
     //}
-      
-    
-
   }
   ngOnInit() {
     this.detForm = this.formBuilder.group({
       desc: ['', Validators.required]
   });
   
+  if (this.service ==="Cab Services") {
+     
+    let cab =['Name : '+this.data.user_id.name,
+    'E-mail : '+this.data.user_id.email,
+    'Pick-up Place : '+this.data.pickup,
+    'Pick-up Date & time : '+this.data.pickup_date,
+    'No of Persons : '+this.data.person,
+    'Vehicle : '+this.data.vehicle,
+    'Destination : '+this.data.destination,
+   "Trip type : "+this.data.trip,
+ "Return Date : "+this.data.return_date]
+
+ this.Details =cab
+    
+  } else if(this.service ==="Construction") {
+    
+let construction=["Name : "+this.data.user_id.name,
+'E-mail : '+this.data.user_id.email,
+"Square Feet : "+this.data.sqrft,
+"Budget :" +this.data.budget,
+"Months : "+this.data.months,
+"Contract : "+this.data.contract,
+"Area : "+this.data.area,
+"Notes : "+this.data.notes
+]
+this.Details =construction
+
+  }
+
+  else if(this.service==="Interiors"){
+
+    let interiors=["Name : "+this.data.user_id.name,
+  "E-mail : "+this.data.user_id.email,
+  "Work Name : "+this.data.work,
+  "Feet x Feet : "+this.data.feet,
+  "Budget : "+this.data.budget,
+"Time : "+this.data.time,
+"Address : "+this.data.address]
+
+  }
 }
 
 
